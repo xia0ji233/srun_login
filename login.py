09cdde893bb80389483030d19741a31acabc917a
xia0ji233 <2636876456@qq.com>
2023-03-24 20:32:58 +0800

    Second Update

--- login.py
+++ login.py
@@ -9,14 +9,29 @@ from encryption.srun_xencode import *
 header={
 	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36'
 }
-init_url="http://10.110.74.91"
-get_challenge_api="http://10.110.74.91/cgi-bin/get_challenge"
 
-srun_portal_api="http://10.110.74.91/cgi-bin/srun_portal"
+init_url="http://10.110.74.91"
+get_challenge_api=''
+srun_portal_api=''
 n = '200'
 type = '1'
 ac_id='4'
+#WLAN:8
 enc = "srun_bx1"
+
+def get_url():
+	global init_url,ac_id
+	url='http://23.215.102.161:80/redirect'
+	p=requests.get(url,headers=header)
+	parttern='<a href="(.*?)">'
+	match=re.search(parttern,p.text)
+	if match==None:
+		return 'Fail'
+	url=match.group(1)
+	init_url='http://'+re.search('[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}',url).group(0)
+	ac_id=re.search('index_(.*?).html',url).group(1)
+	return 'OK'
+
 def get_chksum():
 	chkstr = token+username
 	chkstr += token+hmd5
@@ -40,6 +55,8 @@ def get_info():
 def init_getip():
 	global ip
 	init_res=requests.get(init_url,headers=header)
+	print("初始化获取ip")
+	#print(init_res.text)
 	ip=re.search('ip     : "(.*?)"',init_res.text).group(1)
 	print("ip:",ip)
 def get_token():
@@ -53,17 +70,18 @@ def get_token():
 	}
 	get_challenge_res=requests.get(get_challenge_api,params=get_challenge_params,headers=header)
 	token=re.search('"challenge":"(.*?)"',get_challenge_res.text).group(1)
-	print("token:"+token)
+	#print(get_challenge_res.text)
+	print("token为:"+token)
 def do_complex_work():
 	global i,hmd5,chksum
 	i=get_info()
 	i="{SRBX1}"+get_base64(get_xencode(i,token))
 	hmd5=get_md5(password,token)
 	chksum=get_sha1(get_chksum())
-	print("encode done!")
+	print("所有加密工作已完成")
 def login():
 	srun_portal_params={
-	'callback': 'jQuery11240645308969735664_'+str(int(time.time()*1000)),
+	'callback': 'jQuery112404450565644662372'+str(int(time.time()*1000)),
 	'action':'login',
 	'username':username,
 	'password':'{MD5}'+hmd5,
@@ -80,7 +98,6 @@ def login():
 	}
 	#print(srun_portal_params)
 	srun_portal_res=requests.get(srun_portal_api,params=srun_portal_params,headers=header)
-
 	if 'ok' in srun_portal_res.text:
 		print('登陆成功')
 	else:
@@ -92,8 +109,11 @@ def login():
 
 if __name__ == '__main__':
 	global username,password
-	username=""#一卡通号加上@运营商地址
-	password=""#密码
+	username="00184111@cmcc"
+	password="294816"
+	get_url()
+	get_challenge_api = init_url + "/cgi-bin/get_challenge"
+	srun_portal_api = init_url + "/cgi-bin/srun_portal"
 	init_getip()
 	get_token()
 	do_complex_work()
